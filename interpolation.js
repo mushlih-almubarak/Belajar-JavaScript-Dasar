5315d1e063b805653144c3bc29c50d4e35c9a9cf
Mushlih Almubarak <mushlih_31@mushlih.cf>
2022-05-27 10:36:16 +0300

    Membuat file interpolation

---
+++ interpolation.js
@@ -0,0 +1,10 @@
+const nama = 'Mushlih';
+console.log(`Nama saya ${nama} Almubarak`);
+
+console.log(`5 + 5 = ${5 + 5}`);
+
+function perkalian(a, b) {
+    return a * b;
+}
+
+console.log(`5 * 5 = ${perkalian(5, 5)}`);
